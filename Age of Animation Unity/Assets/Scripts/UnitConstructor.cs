85c2c40fda0c7edd97c53f5e0e29acc1a7dddbe9
Avery Griffin <averyzanegriffin@gmail.com>
2020-07-03 12:57:11 -0400

    Split up UnitConstrutor method into several small methods.

--- Age of Animation Unity/Assets/Scripts/UnitConstructor.cs
+++ Age of Animation Unity/Assets/Scripts/UnitConstructor.cs
@@ -17,20 +17,35 @@ public class UnitConstructor : MonoBehaviour {
     private Casting cast;
     private Units units;
 
+    string nameEdit1 = "(Clone)";
+    string nameEdit2 = " (UnityEngine.GameObject)";
+
+    string Character1 = "Character1";
+    string Character2 = "Character2";
+    string Character3 = "Character3";
+
 
     void Start ()
     {
         age = GameObject.Find("AgeManager").GetComponent<Age>();
+        units = GameObject.Find("UnitManager").GetComponent<Units>();
         attack = gameObject.GetComponentInChildren<Attack>();
         Health = gameObject.GetComponent<health>();
         pm = gameObject.GetComponent<PlayerMovement>();
         cast = gameObject.GetComponent<Casting>();
-        units = GameObject.Find("UnitManager").GetComponent<Units>();
 
 
-        if (this.gameObject.tag == "Player1") { myAge = age.P1Age; }
+        if (this.gameObject.tag == "Player1")
+        {
+            myAge = age.P1Age;
+            SetupP1UnitNames();
+        }
         
-        if (this.gameObject.tag == "Player2") { myAge = age.P2Age; }
+        else if (this.gameObject.tag == "Player2")
+        {
+            myAge = age.P2Age;
+            SetupP2UnitNames();
+        }
 
 
         if (this.gameObject.name == "Character1")
@@ -38,49 +53,110 @@ public class UnitConstructor : MonoBehaviour {
             SetupUnit1();
         }
 
-        if (this.gameObject.name == "Character2")
+        else if (this.gameObject.name == "Character2")
         {
             SetupUnit2();
         }
 
-        if (this.gameObject.name == "Character3")
+        else if (this.gameObject.name == "Character3")
         {
             SetupUnit3();
         }
     }
 
 
+    private void SetupP1UnitNames()
+    {
+        if (this.gameObject.name.Replace(nameEdit1, nameEdit2) == units.P1character1.ToString())
+        { this.gameObject.name = Character1; }
+
+        if (this.gameObject.name.Replace(nameEdit1, nameEdit2) == units.P1character2.ToString())
+        { this.gameObject.name = Character2; }
+
+        if (this.gameObject.name.Replace(nameEdit1, nameEdit2) == units.P1character3.ToString())
+        { this.gameObject.name = Character3; }
+    }
+
+
+    private void SetupP2UnitNames()
+    {
+        if (this.gameObject.name.Replace(nameEdit1, nameEdit2) == units.P2character1.ToString())
+        { this.gameObject.name = Character1; }
+
+        if (this.gameObject.name.Replace(nameEdit1, nameEdit2) == units.P2character2.ToString())
+        { this.gameObject.name = Character2; }
+
+        if (this.gameObject.name.Replace(nameEdit1, nameEdit2) == units.P2character3.ToString())
+        { this.gameObject.name = Character3; }
+    }
+
+
     private void SetupUnit1()
     {
-        unitType = "melee";
-        attack.currentMeleeDamage = units.eachUnit1Damage[myAge];
-        Health.unitsCurrentHealth = units.eachUnit1Health[myAge];
-        Health.unitsStartingHealth = units.eachUnit1Health[myAge];
-        pm.speed = units.eachUnit1Speed[myAge];
-        cast.unitRange = units.eachUnit1Range[myAge];
+        SetUnitType("melee");
+        SetMeleeDamage(units.eachUnit1Damage[myAge]);
+        SetUnitHealth(units.eachUnit1Health[myAge]);
+        SetUnitSpeed(units.eachUnit1Speed[myAge]);
+        SetUnitRange(units.eachUnit1Range[myAge]);
     }
 
 
     private void SetupUnit2()
     {
         unitType = "ranged";
-        attack.currentMeleeDamage = units.eachUnit2Damage[myAge];
-        attack.currentRangeDamage = units.eachUnit2RangeDamage[myAge];
-        Health.unitsCurrentHealth = units.eachUnit2Health[myAge];
-        Health.unitsStartingHealth = units.eachUnit2Health[myAge];
-        pm.speed = units.eachUnit2Speed[myAge];
-        cast.unitRange = units.eachUnit2Range[myAge];
+        SetMeleeDamage(units.eachUnit2Damage[myAge]);
+        SetRangeDamage(units.eachUnit2RangeDamage[myAge]);
+        SetUnitHealth(units.eachUnit2Health[myAge]);
+        SetUnitSpeed(units.eachUnit2Speed[myAge]);
+        SetUnitRange(units.eachUnit2Range[myAge]);
     }
 
 
     private void SetupUnit3()
     {
         unitType = "power";
-        attack.currentMeleeDamage = units.eachUnit3Damage[myAge];
-        attack.currentRangeDamage = units.eachUnit3RangeDamage[myAge];
-        Health.unitsCurrentHealth = units.eachUnit3Health[myAge];
-        Health.unitsStartingHealth = units.eachUnit3Health[myAge];
-        pm.speed = units.eachUnit3Speed[myAge];
-        cast.unitRange = units.eachUnit3Range[myAge];
+        SetMeleeDamage(units.eachUnit3Damage[myAge]);
+        SetRangeDamage(units.eachUnit3RangeDamage[myAge]);
+        SetUnitHealth(units.eachUnit3Health[myAge]);
+        SetUnitSpeed(units.eachUnit3Speed[myAge]);
+        SetUnitRange(units.eachUnit3Range[myAge]);
     }
+
+
+    private void SetUnitType(string type)
+    {
+        unitType = type;
+    }
+
+
+    private void SetMeleeDamage(int meleeDamage)
+    {
+        attack.currentMeleeDamage = meleeDamage;
+    }
+
+
+    private void SetRangeDamage(int rangeDamage)
+    {
+        attack.currentRangeDamage = rangeDamage;
+    }
+
+
+    private void SetUnitHealth(float unitHealth)
+    {
+        Health.unitsStartingHealth = unitHealth;
+        Health.unitsCurrentHealth = Health.unitsStartingHealth;
+    }
+
+
+    private void SetUnitSpeed(float unitSpeed)
+    {
+        pm.speed = unitSpeed;
+    }
+
+
+    private void SetUnitRange(float unitRange)
+    {
+        cast.unitRange = unitRange;
+    }
+
 }
--- Age of Animation Unity/Assets/Scripts/health.cs
+++ Age of Animation Unity/Assets/Scripts/health.cs
@@ -13,66 +13,21 @@ public class health : MonoBehaviour {
     public GameObject thisUnitsDeathSprite;
 
     score Score;
-    Attack attack;
-    spawn Spawn;
-    Units units;
     characterSpawner Spawner;
     HealthBar HpBar;
-
-    string nameEdit1 = "(Clone)";
-    string nameEdit2 = " (UnityEngine.GameObject)";
-
-    string Character1 = "Character1";
-    string Character2 = "Character2";
-    string Character3 = "Character3";
-    string Character4 = "Character4";
-
-
-    private void Awake()
-    {
-
-    }
 
 
     void Start()
     {
         HpBar = gameObject.GetComponentInChildren<HealthBar>();
         Score = FindObjectOfType<score>();
-        attack = gameObject.GetComponentInChildren<Attack>();
-        Spawn = FindObjectOfType<spawn>();
-        units = FindObjectOfType<Units>();
         Spawner = FindObjectOfType<characterSpawner>();
-
-        if (this.gameObject.tag == "Player1")
-        {
-            if (this.gameObject.name.Replace(nameEdit1, nameEdit2) == units.P1character1.ToString())
-            { this.gameObject.name = Character1; }
-
-            if (this.gameObject.name.Replace(nameEdit1, nameEdit2) == units.P1character2.ToString())
-            { this.gameObject.name = Character2; }
-
-            if (this.gameObject.name.Replace(nameEdit1, nameEdit2) == units.P1character3.ToString())
-            { this.gameObject.name = Character3; }
-        }
-
-        if (this.gameObject.tag == "Player2")
-        {
-            if (this.gameObject.name.Replace(nameEdit1, nameEdit2) == units.P2character1.ToString())
-            { this.gameObject.name = Character1; }
-
-            if (this.gameObject.name.Replace(nameEdit1, nameEdit2) == units.P2character2.ToString())
-            { this.gameObject.name = Character2; }
-
-            if (this.gameObject.name.Replace(nameEdit1, nameEdit2) == units.P2character3.ToString())
-            { this.gameObject.name = Character3; }
-        }
     }
 
 
     void StartHealth()
     {
         unitsCurrentHealth = unitsStartingHealth;
-
     }
 
 
@@ -86,7 +41,6 @@ public class health : MonoBehaviour {
         }
 
         else { HpBar.UpdateUnitsHealthBar(); }
-
     }
 
 
@@ -103,8 +57,6 @@ public class health : MonoBehaviour {
         if (clone.name == "Character1") { Spawner.inGameUnit1Bots.Remove(this.gameObject.name); }
         else if (clone.name == "Character2") { Spawner.inGameUnit2Bots.Remove(this.gameObject.name); }
         else if (clone.name == "Character3") { Spawner.inGameUnit3Bots.Remove(this.gameObject.name); }
-
     }
-    
 
 }
