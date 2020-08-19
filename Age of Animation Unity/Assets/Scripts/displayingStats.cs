398897b7a8cf46b097eb8ece59f948b82058bcc2
Avery Griffin <averyzanegriffin@gmail.com>
2020-08-19 08:10:55 -0400

    Made hp bar vertical.

--- Age of Animation Unity/Assets/Scripts/displayingStats.cs
+++ Age of Animation Unity/Assets/Scripts/displayingStats.cs
@@ -5,27 +5,24 @@ using UnityEngine.UI;
 
 public class displayingStats : MonoBehaviour {
 
-    public Text healthText;
     public Text goldText;
     public Text expText;
 
     public score Score;
-    public baseHealth Health;
 
 
     void Update ()
     {
-        healthText.text = "Health: " + Health.currentHealth;
 
         if (this.gameObject.name == "P1 Health, Experience, Gold Parent")
         {
-            goldText.text = "P1 Gold: " + Mathf.RoundToInt(Score.P1Gold);
-            expText.text = "P1 Exp: " + Mathf.RoundToInt(Score.P1Exp);
+            goldText.text = Mathf.RoundToInt(Score.P1Gold).ToString();
+            expText.text = Mathf.RoundToInt(Score.P1Exp).ToString();
         }
         else if (this.gameObject.name == "P2 Health, Experience, Gold Parent")
         {
-            goldText.text = "P2 Gold: " + Mathf.RoundToInt(Score.P2Gold);
-            expText.text = "P2 Exp: " + Mathf.RoundToInt(Score.P2Exp);
+            goldText.text = Mathf.RoundToInt(Score.P2Gold).ToString();
+            expText.text = Mathf.RoundToInt(Score.P2Exp).ToString();
         }
 
     }
